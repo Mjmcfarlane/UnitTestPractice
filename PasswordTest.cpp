2e68292f7df1450616e5bf5b1f8221317314680d
Mariah McFarlane <mjmcfarlane@csuchico.edu>
2024-04-17 11:48:32 -0700

    Add several mixed case tests

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -14,7 +14,7 @@ class PracticeTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-
+/*
 TEST (PasswordTest, single_letter_password)
 {
 	Password my_password;
@@ -43,12 +43,62 @@ TEST (PasswordTest, empty_password)
 	int actual = my_password.count_leading_characters("");
 	ASSERT_EQ(0, actual);
 }
+*/
 
-/*
-TEST (PasswordTest, no_upper_and_lower_password){
+
+TEST (PasswordTest, lower_then_upper_password){
 	Password my_password;
 	bool both_cases = my_password.has_mixed_case("zZ");
 	ASSERT_EQ(true, both_cases);
 }
-*/
+
+TEST (PasswordTest, upper_then_lower_password){
+	Password my_password;
+	bool both_cases = my_password.has_mixed_case("Zz");
+	ASSERT_EQ(true, both_cases);
+}
+
+TEST (PasswordTest, upper_lower_upper_password){
+	Password my_password;
+	bool both_cases = my_password.has_mixed_case("ZzABCD");
+	ASSERT_EQ(true, both_cases);
+}
+
+TEST (PasswordTest, lower_upper_lower_password){
+	Password my_password;
+	bool both_cases = my_password.has_mixed_case("abcDEFghi");
+	ASSERT_EQ(true, both_cases);
+}
+
+TEST (PasswordTest, upper_password){
+	Password my_password;
+	bool both_cases = my_password.has_mixed_case("ZZ");
+	ASSERT_EQ(false, both_cases);
+}
+
+TEST (PasswordTest, lower_password){
+	Password my_password;
+	bool both_cases = my_password.has_mixed_case("zz");
+	ASSERT_EQ(false, both_cases);
+}
+
+TEST (PasswordTest, symbol_lower_password){
+	Password my_password;
+	bool both_cases = my_password.has_mixed_case("chico_$tate_$tudent");
+	ASSERT_EQ(false, both_cases);
+}
+
+TEST (PasswordTest, symbol_mixed_password){
+	Password my_password;
+	bool both_cases = my_password.has_mixed_case("Chico_$tate_$tudent");
+	ASSERT_EQ(true, both_cases);
+}
+
+TEST (PasswordTest, empt_password){
+	Password my_password;
+	bool both_cases = my_password.has_mixed_case("");
+	ASSERT_EQ(false, both_cases);
+}
+
+
 
